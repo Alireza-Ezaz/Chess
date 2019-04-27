628e2fae3839df4589f32cf1876d4db7ab65e30c
Alireza <sar.ezaz@aut.ac.ir>
2019-04-27 21:58:31 +0430

    Bishop moves now

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,12 +1,63 @@
+import java.lang.Math;
+
 public class Bishop extends ChessMan {
 
-    public Bishop(String color){
+    public Bishop(String color) {
         super(color);
         setName("B");
     }
 
 
     boolean move(int currentx, int currenty, int desx, int desy, Object[][] obj) {
+        boolean b = false;
+        if (Math.abs(desx - currentx) == Math.abs(desy - currenty)) {
+            int xx = desx - currentx;
+            int yy = desy - currenty;
+            if (xx > 0 && yy > 0) {
+                for (int i = 1; i < xx; i++)
+                    if (!(obj[currentx + i][currenty + i] instanceof String)) {
+                        System.out.println("Illigal move");
+                        return false;
+                    }
+                b = true;
+            }
+            if (xx > 0 && yy < 0) {
+                for (int i = 1; i < xx; i++)
+                    if (!(obj[currentx + i][currenty - i] instanceof String)) {
+                        System.out.println("Illigal move");
+                        return false;
+                    }
+                b = true;
+            }
+
+            if (xx < 0 && yy > 0) {
+                for (int i = 1; i < yy; i++)
+                    if (!(obj[currentx - i][currenty + i] instanceof String)) {
+                        System.out.println("Illigal move");
+                        return false;
+                    }
+                b = true;
+            }
+
+            if (xx < 0 && yy < 0) {
+                for (int i = 1; i < -xx; i++)
+                    if (!(obj[currentx - i][currenty - i] instanceof String)) {
+                        System.out.println("Illigal move");
+                        return false;
+                    }
+                b = true;
+            }
+
+        }
+        if (b == true) {
+            obj[desx][desy] = (Bishop) obj[currentx][currenty];
+            setCurrentx(desx);
+            setCurrenty(desy);
+            obj[currentx][currenty] = "  ";
+        }
+        if (b == false)
+            System.out.println("Illigal move");
+
         return false;
     }
 
