4b6ac2b9b248427325209727dd98bdaa030d99f0
Alireza <sar.ezaz@aut.ac.ir>
2019-04-27 20:53:34 +0430

    Knight moves now!

--- src/Knight.java
+++ src/Knight.java
@@ -1,12 +1,34 @@
 public class Knight extends ChessMan {
 
-    public Knight(String color){
+    public Knight(String color) {
         super(color);
         setName("K");
     }
 
 
     boolean move(int currentx, int currenty, int desx, int desy, Object[][] obj) {
+        boolean b = false;
+
+        if (currentx - 2 == desx && (currenty + 1 == desy || currenty - 1 == desy))
+            b = true;
+        if (currentx + 2 == desx && (currenty + 1 == desy || currenty - 1 == desy))
+            b = true;
+        if (currenty + 2 == desy && (currentx + 1 == desx || currentx - 1 == desx))
+            b = true;
+        if (currenty - 2 == desy && (currentx + 1 == desx || currentx - 1 == desx))
+            b = true;
+//
+
+        if (b == true) {
+
+            obj[desx][desy] = (Knight) obj[currentx][currenty];
+            setCurrentx(desx);
+            setCurrenty(desy);
+            obj[currentx][currenty] = "  ";
+        }
+
+        if (b == false)
+            System.out.println("Illigal move!!");
         return false;
     }
 
