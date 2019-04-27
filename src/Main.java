e147d3ccbe3086a4c05f226a3ccc4af0919f5013
Alireza <sar.ezaz@aut.ac.ir>
2019-04-27 23:11:36 +0430

    King movement and rules must be coded

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,18 @@ public class Main {
 
         while (true) {
             court.displayBoard();
-            System.out.println("Enter x and y for the chess man you want to move");
+            System.out.println("Enter x and y for the chessman you want to move");
             int currentx = scan.nextInt();
             int currenty = scan.nextInt();
             System.out.println("Enter x and y of destination");
             int desx = scan.nextInt();
             int desy = scan.nextInt();
+
             switch (board[currentx][currenty].toString()) {
                 case "PB":
                 case "PW": {
                     Pawn p = (Pawn) board[currentx][currenty];
-                    System.out.println(p.getColor() + " " + p.getCurrentx() + " " + p.getCurrenty());
+                    //System.out.println(p.getColor() + " " + p.getCurrentx() + " " + p.getCurrenty());
                     if (p.move(currentx, currenty, desx, desy, board) == false && board[desx][desy] instanceof String)
                         continue;
                     if (p.capture(currentx, currenty, desx, desy, board) == false)
@@ -28,7 +29,29 @@ public class Main {
 
 
                 case "KW":
-                case "kB": {
+                case "KB": {
+                    Knight k = (Knight) board[currentx][currenty];
+                    if(k.move(currentx, currenty, desx, desy, board) == false)
+                        continue;
+                }
+                case "BW":
+                case "BB":{
+                    Bishop b =(Bishop)board[currentx][currenty];
+                    if(b.move(currentx, currenty, desx, desy, board) == false)
+                        continue;
+                }
+                case "RW":
+                case "RB":{
+                    Rook r =(Rook) board[currentx][currenty];
+                    if(r.move(currentx, currenty, desx, desy, board) == false)
+                        continue;
+
+                }
+                case "QW":
+                case "QB":{
+                    Queen q=(Queen) board[currentx][currenty];
+                    if(q.move(currentx, currenty, desx, desy, board) == false)
+                        continue;
 
                 }
             }
